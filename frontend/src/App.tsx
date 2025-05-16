df602b8d0ac92bf9a7e1e52eb70686bfdaeae7d4
SandoBall10 <sandovalballona@gmail.com>
2025-05-16 09:41:26 -0500

    borrar carpetas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,} from 'react-router-dom';
 import Login from './components/Login';
 import Principal from './components/Principal';
 import Registro from './components/Registro/Registro';
 import Vendedores from './components/Guia/Vendedores';
 import Compradores from './components/Guia/Compradores';
 import Conocenos from './components/Guia/Conocenos';
+import Favoritos from './components/Opciones/Favoritos';
+import Publicaciones from './components/Opciones/Publicaciones';
+import Perfil from './components/Opciones/Perfil';
 
 const App: React.FC = () => {
   return (
@@ -18,6 +21,9 @@ const App: React.FC = () => {
       <Route path="/vendedores" element={<Vendedores />} />
       <Route path="/compradores" element={<Compradores />} />
       <Route path="/conocenos" element={<Conocenos />} />
+      <Route path="/favoritos" element={<Favoritos />} />
+      <Route path="/publicaciones" element={<Publicaciones />} />
+      <Route path="/perfil" element={<Perfil />} />
     </Routes>
   );
 };
